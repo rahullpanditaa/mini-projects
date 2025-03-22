2c55c47cd7159b21714b3e52da42084aa69d4b6d
Rahul Pandita <rpandita1999@outlook.com>
2025-03-22 15:06:27 +0530

    initial version of ui; asks for user input; 4 commands available - add,remove,show,exit

--- TaskManager/src/UserInterface.java
+++ TaskManager/src/UserInterface.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class UserInterface {
+    private Scanner scanner;
+    private ProgramLogic taskManager;
+
+    public UserInterface(Scanner scanner, ProgramLogic taskManager) {
+        this.scanner = scanner;
+        this.taskManager = taskManager;
+    }
+
+    // start user interface
+    public void start() {
+        while (true) {
+            System.out.println("Following features are available in TaskManager V 1.0:");
+            System.out.println("[1] - add a task.");
+            System.out.println("[2] - remove a task (only if completed, you lazy bastard).");
+            System.out.println("[3] - show active tasks.");
+            System.out.println("[X] - exit.");
+            String command = scanner.nextLine();
+            if (command.equals("X")) {
+                break;
+            }
+            processCommand(command);
+        }
+    }
 }
